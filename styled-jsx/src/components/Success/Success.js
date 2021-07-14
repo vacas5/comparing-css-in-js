266c70c714832f524ca76aca94781e1d16df5820
vacas5 <russelljanderson@gmail.com>
2021-07-13 19:39:42 -0500

    before ejecting

--- styled-jsx/src/components/Success/Success.js
+++ styled-jsx/src/components/Success/Success.js
@@ -1,15 +1,34 @@
 import React from "react";
 import { ReactComponent as Trophy } from "../../icons/trophy.svg";
 import ContentWrapper from "../ContentWrapper/ContentWrapper";
-import "./Success.css";
 
-function Success(props) {
+const GREEN = "green";
+
+function Success() {
   return (
     <ContentWrapper className="success-wrapper">
       <h2 className="success-title">Success!</h2>
       <p className="success-icon">
         <Trophy />
       </p>
+      <style jsx>{`
+        .success-wrapper {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+        }
+
+        .success-title {
+          color: ${GREEN};
+        }
+
+        .success-icon svg {
+          color: ${GREEN};
+          width: 64px;
+          height: 64px;
+        }
+      `}</style>
     </ContentWrapper>
   );
 }
