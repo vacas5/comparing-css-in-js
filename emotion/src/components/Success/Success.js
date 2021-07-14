53e83bbc3c26c1548f138feb323dca3a5d3a0499
Russell Anderson <russelljanderson@gmail.com>
2021-07-13 19:05:52 -0500

    refactor with emotion (#3)

--- emotion/src/components/Success/Success.js
+++ emotion/src/components/Success/Success.js
@@ -1,13 +1,30 @@
-import React from "react";
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react/macro";
 import { ReactComponent as Trophy } from "../../icons/trophy.svg";
 import ContentWrapper from "../ContentWrapper/ContentWrapper";
-import "./Success.css";
 
-function Success(props) {
+const GREEN = "#97C05C";
+
+const wrapper = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const icon = css`
+  svg {
+    color: ${GREEN};
+    width: 64px;
+    height: 64px;
+  }
+`;
+
+function Success() {
   return (
-    <ContentWrapper className="success-wrapper">
-      <h2 className="success-title">Success!</h2>
-      <p className="success-icon">
+    <ContentWrapper css={wrapper}>
+      <h2 css={{ color: GREEN }}>Success!</h2>
+      <p css={icon}>
         <Trophy />
       </p>
     </ContentWrapper>
--- vanilla/src/components/ButtonNavList/ButtonNavList.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function ButtonNavList(props) {
-  return <div></div>;
-}
-
-export default ButtonNavList;
